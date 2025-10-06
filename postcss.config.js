e9ffdf065f3cf57628e128fc0f74a08562320bf0
chanikya23 <chanikyasatish23@gmail.com>
2025-10-06 15:40:39 +0530

    fixing styling issue

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
+// export default {
+//   plugins: {
+//     '@tailwindcss/postcss': {},
+//     autoprefixer: {},
+//   },
+// }
+
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+  ],
+};
+
