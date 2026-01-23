b592130e7eaf4317d6f36ed9ba7490a177a5cf7b
chanikya23 <chanikyasatish23@gmail.com>
2026-01-23 11:44:20 +0530

    skill update

--- client/src/components/SkillsSection.tsx
+++ client/src/components/SkillsSection.tsx
@@ -13,21 +13,21 @@ const skills = [
     { name: "CSS3", icon: SiCss3, color: "#1572B6" },
     { name: "JavaScript", icon: SiJavascript, color: "#F7DF1E" },
     { name: "React.js", icon: SiReact, color: "#61DAFB" },
-    { name: "Next.js", icon: SiNextdotjs, color: "#000000" },
+    { name: "Next.js", icon: SiNextdotjs, color: "#f9f7f7ff" },
     { name: "TypeScript", icon: SiTypescript, color: "#3178C6" },
     { name: "Tailwind CSS", icon: SiTailwindcss, color: "#06B6D4" },
     { name: "Material UI", icon: SiMui, color: "#007FFF" },
 
     // Backend
     { name: "Node.js", icon: SiNodedotjs, color: "#339933" },
-    { name: "Express.js", icon: SiExpress, color: "#000000" },
+    { name: "Express.js", icon: SiExpress, color: "#9f2b2bff" },
     { name: "MongoDB", icon: SiMongodb, color: "#47A248" },
     // { name: "PostgreSQL", icon: SiPostgresql, color: "#4169E1" },
     { name: "Python", icon: SiPython, color: "#3776AB" },
 
     // Tools
     { name: "Git", icon: SiGit, color: "#F05032" },
-    { name: "GitHub", icon: SiGithub, color: "#181717" },
+    // { name: "GitHub", icon: SiGithub, color: "#181717" },
     // { name: "Docker", icon: SiDocker, color: "#2496ED" },
     { name: "Postman", icon: SiPostman, color: "#FF6C37" },
     { name: "VS Code", icon: FaCode, color: "#007ACC" },
